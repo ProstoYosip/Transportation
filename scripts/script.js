26997e82040f47c31e7d5294f88bc5d0ae63519f
ProstoYosip <106190672+ProstoYosip@users.noreply.github.com>
2023-05-22 17:42:26 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// Class JS
+var addClass = document.getElementsByTagName("p");
+addClass[1].classList.add("mail");
+addClass[0].classList.add("phoneNumber");
+
+$(".service").click(function(){
+	$(this).toggleClass("service-submit");
+})
+
+// var footerText = document.getElementsByTagName("p"); 
+// var phoneNumber = document.getElementsByTagName("p"); 
+// footerText[26].classList.add("footerp1");
+// footerText[27].classList.add("footerp2");
+// phoneNumber[28].classList.add("footer-phoneNumber");
+// phoneNumber[29].classList.add("footer-phoneNumber");
+
